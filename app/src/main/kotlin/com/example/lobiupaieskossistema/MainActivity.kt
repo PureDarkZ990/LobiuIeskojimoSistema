bbabf6f34b3daa859ec66fc5d023e7dbbc58ad0f
RobbyGob <dzibar@ktu.lt>
2024-11-17 18:48:50 +0200

    Login and register

---
+++ app/src/main/kotlin/com/example/lobiupaieskossistema/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lobiupaieskossistema
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.login)
+
+    }
+}
