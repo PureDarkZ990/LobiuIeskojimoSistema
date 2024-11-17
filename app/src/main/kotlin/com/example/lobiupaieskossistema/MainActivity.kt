6fd5e8c7633f44cbe95000a46442d0feb7231a72
RobbyGob <dzibar@ktu.lt>
2024-11-17 20:18:31 +0200

    Profile

--- app/src/main/kotlin/com/example/lobiupaieskossistema/MainActivity.kt
+++ app/src/main/kotlin/com/example/lobiupaieskossistema/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.lobiupaieskossistema
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.login)
+        setContentView(R.layout.profile)
 
+        val imageView: ImageView = findViewById(R.id.profileImage)
+
+        try {
+            val assetManager = assets
+            val inputStream: InputStream = assetManager.open("default_profile_image.jpg")
+            val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
+            imageView.setImageBitmap(bitmap)
+        } catch (e: IOException) {
+            e.printStackTrace() // Handle the exception if the image is not found
+        }
     }
 }
