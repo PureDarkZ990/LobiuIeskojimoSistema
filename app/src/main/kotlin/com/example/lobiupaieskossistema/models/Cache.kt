f4cc21882b8a274aa6593709a28d216de4ca8a75
PureDarkZ990 <mincalka@gmail.com>
2024-12-22 15:11:38 +0200

    harcoded cache

--- app/src/main/kotlin/com/example/lobiupaieskossistema/models/Cache.kt
+++ app/src/main/kotlin/com/example/lobiupaieskossistema/models/Cache.kt
@@ -11,10 +11,11 @@ data class Cache(
     var rating: Double? =0.0,
     var difficulty: Double?,
     var approved: Int? = 0,
-    var createdAt: String?,
-    var updatedAt: String?,
+    var createdAt: String? = "2024-01-01T00:00:00Z", // Default value
+    var updatedAt: String? = "2024-01-01T00:00:00Z", // Default value
     var private: Int = 0,
     var shown: Int? = 1,
     var themeId: Int? = null,
     var creatorId: Int?
+
 )
