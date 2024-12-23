0984401da1346b879f716f24f81593bd54c7a197
RobbyGob <dzibar@ktu.lt>
2024-12-23 10:32:18 +0200

    Profile Is almost Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     //noinspection GradleDependency
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.maps.android:android-maps-utils:3.4.0")
 
 }
